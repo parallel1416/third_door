77a81602576b764ac78597a80ffab020647cde2b
Ing Ventaka <110343080+parallel1416@users.noreply.github.com>
2024-05-29 14:57:01 +0800

    init vite react spline app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline';
+import LandingPage from './landingpage/LandingPage'
+
+export default function App() {
+  return (
+    <>
+      <Spline scene="https://prod.spline.design/PqfwnF1tpbeAl7E0/scene.splinecode" />
+      <LandingPage />
+    </>
+  );
+}
